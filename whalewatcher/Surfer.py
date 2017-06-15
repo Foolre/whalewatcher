9dd5d7a98b918e32feaf5275a70595efc641aa23
RoelofN <rudy.nutbeij@student.hu.nl>
2017-06-15 00:32:57 +0000

    derp

--- whalewatcher/Surfer.py
+++ whalewatcher/Surfer.py
@@ -34,46 +34,52 @@ ACCOUNT_COMMANDS = '''{
     "getdeposithistory": "currency"
 }'''
 
-def btrequest(url):
+def btrequest(uri):
     time.sleep(1)
-    uri='https://bittrex.com/api/v1.1{0}'
+    url='https://bittrex.com/api/v1.1{0}'
     headers = { 'Content-Type': 'application/json' }
-    headers['apisign'] = hmac.new(secret.encode(), uri.format(url).encode(), hashlib.sha512).hexdigest() 
-    #print("requesting {0}".format(uri.format(url)))
-    req = Request(uri.format(url), headers=headers)
+    headers['apisign'] = hmac.new(secret.encode(), url.format(uri).encode(), hashlib.sha512).hexdigest() 
+    req = Request(url.format(uri), headers=headers)
     response = urlopen(req).read().decode()
     parsed = json.loads(response)
-    return parsed["result"]
+    return parsed['result']
 
-def comparebidpaid(bid, paid):
-    result=((bid - paid ) / ((bid + paid) / 2)) * 100
-    if result > float(0):
+def comparebidpaid(marketbid, pricepaid):
+    result=((marketbid - pricepaid ) / ((marketbid + pricepaid) / 2)) * 100
+    if result > float(5):
         return True
     else:
         return False
 
+def nonce(uri):
+    '''calculates this nonce thingy and puts together the uri string'''
+    result = uri + '&nonce=' + str(int(time.time() * 1000 )) + '&apikey=' + apikey
+    return result
+
+uri = nonce('/account/getbalance?currency={0}')
+balance = {'btc': btrequest(uri.format('btc')),
+           'eth': btrequest(uri.format('eth'))
+          }
+
+uri = nonce('/account/getorderhistory?market={0}')
+history = btrequest(uri.format('btc-eth'))
+lastpurchase = history[0]
+if lastpurchase['OrderType'] == 'LIMIT_BUY':
+    #bought eth paid with btc
+    ethquantity = lastpurchase['Quantity'] - lastpurchase['Commission']
+    btcquantity = lastpurchase['Price']
+    pricepaid = float(btcquantity / ethquantity)
+
+uri = '/public/getmarketsummary?market={0}'
+sell = '/market/selllimit?market={0}&quantity={1}&rate={2}'
 while True:
-    nonce = '&nonce='+urllib.parse.quote(str(int(time.time() * 1000))) + '&apikey=' + apikey
-    uri = '/account/getbalance?currency={0}' + nonce
-    balance = {}
-    balance['btc'] = btrequest(uri.format('btc'))
-    balance['eth'] = btrequest(uri.format('eth'))
-    uri = '/public/getmarketsummary?market=BTC-ETH'
-    market = btrequest(uri)
-    uri = '/account/getorderhistory?market=BTC-ETH' + nonce
-    history = btrequest(uri)
-    print(history)
-    exit(1)
-    if history['OrderType'] == "LIMIT_BUY":
-        unitpricepaid = history["PricePerUnit"]
-        amountsell = history["Price"]
-        amountbuy = history["Quantity"]
-        available = balance['eth']['Available']
-        unitbid = market[0]['Bid']
-        difference = float((amountbuy * unitbid ) - amountsell)
-        if comparebidpaid(unitbid, unitpricepaid):
-            print('profit: btc {0:f}'.format(difference))
-        else:
-            loss = float((amountbuy * unitbid ) - amountsell)
-            print('loss btc {0:f}'.format(difference))
+    market = btrequest(uri.format('btc-eth'))
+    marketbid = market[0]['Bid']
+    difference = marketbid - pricepaid
+    if comparebidpaid(marketbid, pricepaid):
+        order = sell.format('btc-eth', ethquantity, marketbid)
+        print('profit: btc {0:f}'.format(difference))
+        exit(1)
+    else:
+        print('loss btc {0:f}'.format(difference))
     time.sleep(5)
